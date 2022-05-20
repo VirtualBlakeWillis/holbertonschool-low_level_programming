e06f3262ab9556bae6940cba61ac528f475968c5
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-20 15:40:38 -0700

    d dada type to lu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
   */
 int main(void)
 {
-	printf("Size of a char: %d\n", sizeof(char));
-	printf("Size of an int: %d\n", sizeof(int));
-	printf("Size of a long int: %d\n", sizeof(long int));
-	printf("Size of a long long int: %d\n", sizeof(double));
-	printf("Size of a float: %d\n", sizeof(float));
+	printf("Size of a char: %lu\n", sizeof(char));
+	printf("Size of an int: %lu\n", sizeof(int));
+	printf("Size of a long int: %lu\n", sizeof(long int));
+	printf("Size of a long long int: %lu\n", sizeof(double));
+	printf("Size of a float: %lu\n", sizeof(float));
 
 	return (0);
 }
