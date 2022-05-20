99131f2d9605ce60eaa07e538af58e53a7569816
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-20 15:37:55 -0700

    data types!

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,18 @@
+#include <studio.h>
+#include <string.h>
+/**
+  *main - prints size of data types for system running program
+  *
+  *Return: 0
+  */
+init main(void)
+{
+	printf("Size of a char: %d\n", sizeof(char));
+	printf("Size of an int: %d\n", sizeof(int));
+	printf("Size of a long int: %d\n", sizeof(long int));
+	printf("Size of a long long int: %d\n", sizeof(double));
+	printf("Size of a float: %d\n", sizeof(float));
 
+	return (0);
+}
 
