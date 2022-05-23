907f0466a7c7c780f65796eb8a2418c88cddb5ea
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-23 12:30:56 -0700

    made 9 more readable

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+  * main -
+  *
+  * Return:
+  */
+int main(void)
+{
+	int i, x, inasc_one, inasc_two, comma, space;
+
+	for (i = 0; i < 10; i++)
+	{
+		for(x = 0; x < 10; x++)
+		{
+			if (x != i && x > i )
+			{
+				inasc_one = i + 48;
+				inasc_two = x + 48;
+				putchar(inasc_one);
+				putchar(inasc_two);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,8 +14,8 @@ int main(void)
 		putchar(i);
 		if (i != 57)
 		{
-			putchar(44);
-			putchar(32);
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
