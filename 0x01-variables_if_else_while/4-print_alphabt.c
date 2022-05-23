5d2dcd4844dc5a6641a3aa878e46e11563668a66
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-23 10:36:41 -0700

    betty compliant, logic or operators, is not operator

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - print alphabet, exclude two characters
+  *
+  * Return: return 0
+  */
+int main(void)
+{
+	int i, x;
+
+	for (i = 97; i < 123; i++)
+	{
+		x = i;
+		if (x != 'q' || x != 'e')
+			putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
+
