a70df9e35e7a5d89e4c64c038fb3c165367e678c
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-24 12:38:08 -0700

    loops and if statements!

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+  * jack_bauer - prints every minute of hours 0-23
+  * Return: void
+  */
+
+void jack_bauer(void)
+{
+
+	int h, m, tmp; /* Vars for Counting */
+	int ha, hb, ma, mb; /* Vars for display */
+	for (h = 0; h <= 23; h++)
+	{
+		
+		for (m = 0; m <= 59; m++)
+		{
+			if (h < 10)
+			{
+				ha = 0;
+				hb = h;
+			}
+			else
+			{
+				hb = h % 10;
+				tmp = h - hb;
+				ha = tmp / 10;
+			}
+			if (m < 10)
+			{
+				ma = 0;
+				mb = m;
+			}
+			else
+			{
+				mb = m % 10;
+				tmp = m - mb;
+				ma = tmp / 10;
+			}
+			_putchar(ha + '0');
+			_putchar(hb + '0');
+			_putchar(':');
+			_putchar(ma + '0');
+			_putchar(mb + '0');
+			_putchar('\n');
+		}
+	}
+}
