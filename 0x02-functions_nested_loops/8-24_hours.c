6410710c1516d56bbaf00223e87a8feee9706540
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-24 12:39:25 -0700

    betty compliant

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,10 @@ void jack_bauer(void)
 
 	int h, m, tmp; /* Vars for Counting */
 	int ha, hb, ma, mb; /* Vars for display */
+
 	for (h = 0; h <= 23; h++)
 	{
-		
+
 		for (m = 0; m <= 59; m++)
 		{
 			if (h < 10)
