f7b74cfa5cbd8189dd2760a7d668bbc892550c55
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-24 11:53:17 -0700

    attempts 1

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * print_last_digit - modulo 10 the input, prints last digit
+  *@x: input int
+  *@z: output digit
+  *@Return: output(z) last digit of input(x)
+  */
+int print_last_digit(int x)
+{
+	int z;
+
+	z = x % 10;
+	return (z);
+}
