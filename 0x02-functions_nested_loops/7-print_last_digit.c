e25407792197e60a7255983f1113fe46534dcdc9
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-24 12:01:30 -0700

    weird

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
+#include "main.h"
 /**
   * print_last_digit - modulo 10 the input, prints last digit
   *@x: input int
-  *@z: output digit
-  *@Return: output(z) last digit of input(x)
+  *Return: output(z) last digit of input(x)
   */
 int print_last_digit(int x)
 {
-	int z;
-
-	z = x % 10;
+	char z;
+	z = (x % 10) + 48;
+	_putchar(z);
 	return (z);
 }
