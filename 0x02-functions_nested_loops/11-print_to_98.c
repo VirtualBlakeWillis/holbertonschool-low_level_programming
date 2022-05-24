cfa34d42821702e193e8a3953a5ce5b76d28a3c5
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-24 13:38:39 -0700

    spaces

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			printf("%d, ",n);
+			printf("%d, ", n);
 			n++;
 		}
 		printf("98\n");
@@ -19,11 +19,11 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			printf("%d, ",n);
+			printf("%d, ", n);
 			n--;
 		}
 		printf("98\n");
 	}
 	else
-		printf("%d\n",n);
+		printf("%d\n", n);
 }
