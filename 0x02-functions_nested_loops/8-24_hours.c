cfd8c3aff27af767711668a3a25c41a8f2bc36fc
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-25 08:29:06 -0700

    made code cleaner

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,41 +7,18 @@
 void jack_bauer(void)
 {
 
-	int h, m, tmp; /* Vars for Counting */
-	int ha, hb, ma, mb; /* Vars for display */
+	int h, m; /* Vars for Counting */
 
 	for (h = 0; h <= 23; h++)
 	{
 
 		for (m = 0; m <= 59; m++)
 		{
-			if (h < 10)
-			{
-				ha = 0;
-				hb = h;
-			}
-			else
-			{
-				hb = h % 10;
-				tmp = h - hb;
-				ha = tmp / 10;
-			}
-			if (m < 10)
-			{
-				ma = 0;
-				mb = m;
-			}
-			else
-			{
-				mb = m % 10;
-				tmp = m - mb;
-				ma = tmp / 10;
-			}
-			_putchar(ha + '0');
-			_putchar(hb + '0');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(':');
-			_putchar(ma + '0');
-			_putchar(mb + '0');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
 		}
 	}
