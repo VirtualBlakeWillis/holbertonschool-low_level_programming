75dc202a86ec1a7bda913cd84e54ca111759d4a1
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-30 13:55:54 -0700

    squares!

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_square - create a squre, n by n characters wide/long
+  *@size: paramaters for width and legnth
+  *Return: void
+  */
+
+void print_square(int size)
+{
+	int i, x, n;
+
+	n = size;
+	for (i = 0; i < n; i ++)
+	{
+		for (x = 0; x < n; x++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
+
