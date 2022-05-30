96c316ec7d5c5609903fa7452794cbe77013e62b
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-30 13:00:16 -0700

    betty compliant

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,13 +2,13 @@
 
 /**
   * print_line - print n _'s, where n = input
-  * n@ number of _'s to print
+  * @n: number of _'s to print
   * Return: void
   */
 
 void print_line(int n)
 {
-	for(; n > 0; n--)
+	for (; n > 0; n--)
 		_putchar('_');
 	_putchar('\n');
 
