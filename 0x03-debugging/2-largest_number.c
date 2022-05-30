24d1c573220d8f221b52d43cfa547429899986f5
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-30 10:56:58 -0700

    betty compliant

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,25 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else if (c > a && c > b)
-    {
-        largest = c;
-    }
-    else if (a > c)
-	    largest = a;
-    else if (b > c)
-	    largest = b;
-    else
-	    largest = c;
-    return (largest);
+	if (a > b && a > c)
+		largest = a;
+	else if (b > a && b > c)
+		largest = b;
+	else if (c > a && c > b)
+		largest = c;
+	else if (a > b)
+		largest = a;
+	else if (b > c)
+		largest = b;
+	else
+		largest = c;
+
+	return (largest);
 }
--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -8,16 +8,16 @@
 
 int main(void)
 {
-        int a, b, c;
-        int largest;
+	int a, b, c;
+	int largest;
 
-        a = 12;
-        b = 12;
-        c = 11;
+	a = 12;
+	b = 12;
+	c = 11;
 
-        largest = largest_number(a, b, c);
+	largest = largest_number(a, b, c);
 
-        printf("%d is the largest number\n", largest);
+	printf("%d is the largest number\n", largest);
 
-        return (0);
+	return (0);
 }
