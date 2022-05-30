1e59aee820290f9c3a26f6e1c4297bc288a41e6f
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-30 12:59:10 -0700

    drawing lines!

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * print_line - print n _'s, where n = input
+  * n@ number of _'s to print
+  * Return: void
+  */
+
+void print_line(int n)
+{
+	for(; n > 0; n--)
+		_putchar('_');
+	_putchar('\n');
+
+}
