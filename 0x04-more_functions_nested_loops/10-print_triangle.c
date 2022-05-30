406200fdfb78688cd892b792b8d189c6a80951c7
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-30 14:34:26 -0700

    betty is happy

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
-  * print_triangle - creates a right angle triangle, with the right angle in the bottom right of input size
-  * @size - how big you want your favorite triangle
+  * print_triangle - creates a right angle triangle
+  * @size: how big you want your favorite triangle
   * Return: void
   */
 
 void print_triangle(int size)
 {
 	int i, x, spc;
-	
+
 	spc = size - 1;
 	if (size > 0)
 	{
