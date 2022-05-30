d1b9d95ed9a614d639d361cf1e07a74e5abdc2d5
VirtualBlakeWillis <bewillis101@gmail.com>
2022-05-30 10:53:08 -0700

    rougher logic

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,10 +20,15 @@ int largest_number(int a, int b, int c)
     {
         largest = b;
     }
-    else
+    else if (c > a && c > b)
     {
         largest = c;
     }
-
+    else if (a > c)
+	    largest = a;
+    else if (b > c)
+	    largest = b;
+    else
+	    largest = c;
     return (largest);
 }
