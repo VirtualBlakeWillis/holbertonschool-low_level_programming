4964b1a7c87cdf8b81f435df588903ea5681e03c
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-01 08:46:58 -0700

    happy feet

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * swap_int - swaps input a's value with input b's value
+  * @a: input a
+  * @b: input b
+  * Return: void
+  */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
