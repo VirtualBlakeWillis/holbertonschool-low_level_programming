aeb55d07075220e8e614570a2534a56223ed3af1
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-01 08:59:55 -0700

    adding putchar!

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+  * _strlen - gets legnth of string
+  * @s: string to get legnth of
+  * Return: int
+  */
+
+int _strlen(char *s);
+{
+	int i[];
+	for (i = &s; i >= 0; i--;)
+	{
+		
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 /* prototypes for 0x05 project */
+int _putchar(char c);
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
