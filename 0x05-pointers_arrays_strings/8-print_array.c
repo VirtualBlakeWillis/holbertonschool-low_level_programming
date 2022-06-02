17c4952fc605d12f898748b3277dedf7e521ee13
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-02 10:50:56 -0700

    printf fun!

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * print_array - print n elements of input array, formatted
+  * @a: input array
+  * @n: number of elements to print
+  */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", *(a + i));
+	}
+	printf("%d\n", *(a + (n - 1)));
+
+}
