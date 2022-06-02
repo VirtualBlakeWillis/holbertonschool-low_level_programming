d01d48caa43ade0d1046d7bab30d638715ba8e82
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-02 13:16:11 -0700

    copying a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * _strcpy - copies the string pointed to by src
+  * to the buffer pointed to by dest.
+  * @dest: buffer
+  * @src pointer
+  * Return: void
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	char *tmp = src;
+
+	while (*src != 0)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return (tmp);
+
+}
