0a9b9662bbd376c4bcc4b43e0bffbec9b90fd8fa
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-06 14:19:48 -0700

    fun with ascii and if statements

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+  *
+  */
+
+char *string_toupper(char *str)
+{
+	int str_len = _strlen(str), i;
+	
+	for (i = 0; i < str_len; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
+
+/**
+* _strlen - gets length of string
+* @s: string to get length of
+* Return: int
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+	while (*s++)
+		i++;
+	return (i);
+}
