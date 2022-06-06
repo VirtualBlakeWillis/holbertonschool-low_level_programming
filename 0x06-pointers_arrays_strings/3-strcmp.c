5808630856e0d285c092063fb901bf4183d62d78
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-06 11:37:24 -0700

    wow

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+/**
+  * _strcmp - compares two strings
+  * @s1: string one
+  * @s2: strint two
+  * Return: 
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;	
+	return (s1[i] - s2[i]);
+}
