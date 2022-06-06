edb0708c7398aa2c7a6ac97f0721a85ed9b80851
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-06 08:07:34 -0700

    add s2 onto the end of s1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+  * _strcat - appends src to the dest string, returning pointer to new dest
+  * @src: string to add to dest
+  * @dest: string being added too
+  * Return: pointer to dest
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = _strlen(dest);	
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
+
+
+/**
+ * _strlen - gets legnth of string
+ * @s: string to get legnth of
+ * Return: int
+ */ 
+int _strlen(char *s)
+{
+	int i = 0;
+	while (*s++)
+		i++;
+	return (i);
+}
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* Prototypes for 0x06 project */
+int _putchar(char c);
+
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+void reverse_array(int *a, int n);
+char *cap_string(char *);
+char *leet(char *);
+
+
+#endif
