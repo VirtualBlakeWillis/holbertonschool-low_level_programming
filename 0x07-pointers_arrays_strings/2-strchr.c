40bc7fe21e98f7fa2f3dc6ffa42279740711fb9f
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-07 10:21:38 -0700

    betty + compiler happy (task 8 not happy)

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,14 @@
 /**
-  *
+  * _strchr - search for char c.
+  * @s: array to search
+  * @c: char looking for
+  * Return: Return loc of c if found, or NULL if not
   */
 
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	char retVal;
+
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
