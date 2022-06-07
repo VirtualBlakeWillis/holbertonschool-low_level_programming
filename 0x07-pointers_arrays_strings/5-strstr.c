67dcc3e95f2febe7035a16d34edf815a5e6e18d1
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-07 15:12:27 -0700

    wowzers!

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,16 +1,39 @@
+#include "main.h"
 #include <stddef.h>
 /**
   *
   */
 
-char *_strstr(char *haystack, char *needle)
+char *_strstr(char *hak, char *ned)
 {
 	unsigned int i = 0;
 	int out = 0;
 	int x;
 	char *startLoc;
-	while (*haystack != '\0')
+
+	while (hak[i] != '\0')
 	{
+		if (hak[i] == ned[0])
+		{
+			startLoc = &hak[i]; /* think about putting at end. val = hak[i - x] */
+
+			for (x = 0; ned[x] != '\0'; x++)
+			{
+				if (hak[i + x] != ned[x])
+					break;
+			}
+		 	
+			if (hak[i + x] == ned[x])
+				return (&hak[i - x]);
+		}
+
+		i++;
+	}
+	return (NULL);
+}
+
+
+/*		
 		if (haystack[i] == needle[0])
 		{
 			startLoc = &haystack[i];
@@ -30,11 +53,4 @@ char *_strstr(char *haystack, char *needle)
 		}
 		if (out == 1)
 			break;
-		i++;
-	}
-
-	if (*haystack == '\0')
-		return (NULL);
-	else
-		return (startLoc);
-}
+*/	
