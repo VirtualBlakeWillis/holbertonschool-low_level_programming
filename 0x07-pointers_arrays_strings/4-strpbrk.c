2251c4eb0cbfd9d125bf4d7d479a2b1c79bedc0a
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-07 11:22:44 -0700

    a pretty first attempt

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+/**
+  *
+  */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int x;
+	int i = 0;
+	int out = 0;
+
+	while (out == 0)
+	{
+		for (x = 0; accept[x] != '\0'; x++)
+		{
+			if (s[i] == accept[x])
+			{
+				out = 1;
+				break;
+			}
+		}
+		if (out == 1)
+			break;
+		else
+			i++;
+	}
+	
+
+	if (s[i] == '\0')
+		return ('\0');
+	else
+		return (s + i);
+}
