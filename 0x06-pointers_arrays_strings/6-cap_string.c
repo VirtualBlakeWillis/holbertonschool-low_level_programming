29832e545e165e2c830f259e33734971f38b32c1
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-06 18:13:19 -0700

    new fxn

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 /**
   * cap_string - capitalize all words of a string (that meet criteria)
   * @s - input string
@@ -6,18 +7,47 @@
 
 char *cap_string(char *str)
 {
-	int i = 0;
+	int str_len = _strlen(str);
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; i < str_len; i++)
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' \
-				|| str[i] == '.' || str[i] == '!' || str[i] == '"' || str[i] == '(' \
-				|| str[i] == ')' || str[i] == '{' || str[i] == '}')
+		if ((str[i] == ' ' || str[i] == '\t') || \
+				(str[i] == '\n' || str[i] == ',') || \
+				(str[i] == ';' || str[i] == '.' ) || \
+		   		(str[i] == '!' || str[i] == '"' ) || \
+				(str[i] == '(' || str[i] == ')' ) || \
+				(str[i] == '{' || str[i] == '}'))
 		{
+			i++;
 			if (str[i] >= 97 && str[i] <= 122)
 				str[i] = str[i] - 32;
 		}
+
 		i++;
 	}
 	return str;
+}
+
+/**
+ * _strlen - gets length of string
+ * @s: string to get length of
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+  * checkFor - checks conditions
+  * @val: input value
+  * Return: true of false
+  */
+bool checkVal(int val)
+{
+
 }
