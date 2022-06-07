9bf572e02313131e56c0cdba74748362871cf34c
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-07 15:14:05 -0700

    cleaner

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,16 +7,13 @@
 char *_strstr(char *hak, char *ned)
 {
 	unsigned int i = 0;
-	int out = 0;
 	int x;
-	char *startLoc;
 
 	while (hak[i] != '\0')
 	{
 		if (hak[i] == ned[0])
 		{
-			startLoc = &hak[i]; /* think about putting at end. val = hak[i - x] */
-
+	
 			for (x = 0; ned[x] != '\0'; x++)
 			{
 				if (hak[i + x] != ned[x])
