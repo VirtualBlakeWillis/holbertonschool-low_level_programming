5e77cbfd11ba3b6f07fa2bee7c50640d67450aef
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-07 08:21:25 -0700

    save

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+/**
+  * _memset - fills the first n bytes of the memory area s with a constant b
+  * @s: pointer to memory area
+  * @b: byte for input
+  * @n: number of bytes of s to change into b
+  * Return: pointer to the memory area s
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return s;
+}
