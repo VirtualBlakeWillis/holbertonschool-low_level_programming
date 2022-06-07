88ef82141237627b8291b932f234b695a91f959f
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-07 12:13:23 -0700

    first try

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include <stddef.h>
+/**
+  *
+  */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0;
+	int out = 0;
+	int x;
+	char *startLoc;
+	while (*haystack != '\0')
+	{
+		if (haystack[i] == needle[0])
+		{
+			startLoc = &haystack[i];
+
+			for (x = 0; needle[x] != '\0'; x++)
+			{
+				if (haystack[i + x] != needle[x])
+					break;
+			}
+
+			if (haystack[i + x] == needle[x])
+				out = 1;
+		}
+		if (out == 1)
+			break;
+		i++;
+	}
+
+	if (*haystack == '\0')
+		return (NULL);
+	else
+		return (startLoc);
+}
