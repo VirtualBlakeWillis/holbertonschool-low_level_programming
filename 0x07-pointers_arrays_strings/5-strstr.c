8f2e7cd5bc16681b654bc0166ed565dbba1e336b
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-09 08:55:10 -0700

    base code fro myesterday

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,35 +1,48 @@
-#include "main.h"
-#include <stddef.h>
+
+
 /**
   * _strstr - return address of first val in ned, if all of ned is in hak
   * @hak: haystack, aka str 1 to check against
   * @ned: needle, aka str 2 to check if in str1
   * Return: address for hak, or NULL
   */
+#include <stddef.h>
+/**
+  *
+  */
 
-char *_strstr(char *hak, char *ned)
+char *_strstr(char *haystack, char *needle)
 {
-	int i, x;
-
-	for (i = 0; hak[i] != '\0'; ++i)
+	unsigned int i = 0;
+	int out = 0;
+	int x;
+	char *startLoc;
+	while (*haystack != '\0')
 	{
-		
-		if (hak[i] == ned[0])
+		if (haystack[i] == needle[0])
 		{
-			x = 0;
-			while (ned[x] != '\0')	
+			startLoc = &haystack[i];
+
+			for (x = 0; needle[x] != '\0'; x++)
 			{
-				if (hak[i + x] != ned[x])
+				if (haystack[i + x] != needle[x])
 					break;
-				else
-					x++;
 			}
-			if (hak[i + x] == ned[x])
-				return (&hak[i]);
+
+			if (haystack[i + x] == needle[x])
+			{
+				out = 1;
+				break;
+			}
+			startLoc = &haystack[0];
 		}
-		
+		if (out == 1)
+			break;
+		i++;
 	}
 
-	return ('\0');
+	if (*haystack == '\0')
+		return (NULL);
+	else
+		return (startLoc);
 }
-
