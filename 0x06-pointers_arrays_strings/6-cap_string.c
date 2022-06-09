ea2e28c0d6c75334c53a3c8bda1a2f9b7ecd8741
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-09 08:26:45 -0700

    system cleanup

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,16 +12,10 @@ char *cap_string(char *str)
 
 	for (i = 0; i < str_len; i++)
 	{
-		if ((str[i] == ' ' || str[i] == '\t') || \
-				(str[i] == '\n' || str[i] == ',') || \
-				(str[i] == ';' || str[i] == '.' ) || \
-		   		(str[i] == '!' || str[i] == '"' ) || \
-				(str[i] == '(' || str[i] == ')' ) || \
-				(str[i] == '{' || str[i] == '}'))
+		if (checkFor(str[i]) == 1)
 		{
-			i++;
-			if (str[i] >= 97 && str[i] <= 122)
-				str[i] = str[i] - 32;
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				str[i + 1] = str[i+ 1] - 32;
 		}
 
 		i++;
@@ -47,7 +41,51 @@ int _strlen(char *s)
   * @val: input value
   * Return: true of false
   */
-bool checkVal(int val)
+int checkFor(int val)
 {
-
+	switch (val)
+	{
+		case 9:
+			return 1;
+			break;
+		case 10:
+			return 1;
+			break;
+		case 32:
+			return 1;
+			break;
+		case 33:
+			return 1;
+			break;
+		case 34:
+			return 1;
+			break;
+		case 40:
+			return 1;
+			break;
+		case 41:
+			return 1;
+			break;
+		case 44:
+			return 1;
+			break;
+		case 46:
+			return 1;
+			break;
+		case 59:
+			return 1;
+			break;
+		case 63:
+			return 1;
+			break;
+		case 123:
+			return 1;
+			break;
+		case 125:
+			return 1;
+			break;
+		default:
+			return 0;
+			break;
+	}
 }
