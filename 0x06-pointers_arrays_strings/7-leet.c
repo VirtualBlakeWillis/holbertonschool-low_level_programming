501db99faad56e2f744f7aae0a5dbd88b2de0231
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-09 12:16:57 -0700

    eleet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,20 @@
+/**
+  *
+  */
+
+char *leet(char *str)
+{
+	char letter[] = "aAeEoOtTlL";
+	char number[] = "4433007711";
+	int i = 0, j;
+	while (str[i])
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == letter[j])
+				str[i] = number[j];
+		}
+		i++;
+	}
+	return (str);
+}
