43a7c00548612cc02b7d9a4ebb9e3663934f44d0
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-09 12:18:56 -0700

    eleet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,5 +1,7 @@
 /**
-  *
+  * *leet - replace some letters with numbers in a string
+  * @str: input string
+  * Return: pointer to str
   */
 
 char *leet(char *str)
@@ -7,6 +9,7 @@ char *leet(char *str)
 	char letter[] = "aAeEoOtTlL";
 	char number[] = "4433007711";
 	int i = 0, j;
+
 	while (str[i])
 	{
 		for (j = 0; j < 10; j++)
