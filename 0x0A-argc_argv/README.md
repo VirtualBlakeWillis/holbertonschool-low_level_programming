c92b49653115cd2f775d280bddcd359899d2e221
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-14 08:17:10 -0700

    a happy skeleton

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+# Project 1130: 0x0B. C - argc, argv
+## Link To Project: [Click Here!](https://intranet.hbtn.io/projects/1130)
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+// Standard Libraries
+#include <stdio.h>
+#include <stdlib.h>
+
+// Project Prototypes
+
+
+
+#endif
