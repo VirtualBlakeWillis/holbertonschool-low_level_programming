82be151f4355834eb3e0e259af9aeedd48b47209
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-14 08:25:51 -0700

    comments 4 betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+  * main - print name of function
+  * @argc: # of input arguments
+  * @argv: pointer array of input arguments
+  * Return: 0
+  */
+
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+  * main - print # of input arguments (argc - 1)
+  * @argc: # of input arguments
+  * @argv: pointer array of input arguments
+  * Return: 0
+  */
 
 int main(int argc, char *argv[])
 {
