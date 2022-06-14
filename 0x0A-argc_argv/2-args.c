2e76af7e8bc955bfbeb73c92450a46d5fecb0183
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-14 08:28:52 -0700

    prints every value of argv[]

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - print values in argv[], each followed by a new line
+  * @argc: # of input arguments
+  * @argv: pointer array of input arguments
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+
+	if (argc > 0)	
+	{
+		for (int i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
+	}
+
+	return(EXIT_SUCCESS);
+}
