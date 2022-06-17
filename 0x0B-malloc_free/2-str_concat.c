8ac1787d5775d64c43dbde64dfc4adf578eeb560
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-16 19:07:07 -0700

    combining strings!

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+  * str_concat - allocate a new space in memory, holding the contents of s1+s2
+  * @s1: input string 1
+  * @s2: input string 2
+  * Return: pointer to a new space in memory, or NULL
+  */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i = 0, x = 0, z = 0, str_len;
+	char *p;
+
+	while (s1[i])
+		i++;
+
+	while (s2[x])
+		x++;
+
+	str_len = i + x;
+
+	p = malloc(str_len * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+
+	while (z < i)
+	{
+		p[z] = s1[z];
+		z++;
+	}
+	z = 0;
+
+	while (z < x)
+	{
+		p[i + z] = s2[z];
+		z++;
+	}
+	p[i + x] = '\0';
+
+	return (p);
+
+}
