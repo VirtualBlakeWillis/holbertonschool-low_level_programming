5538ff84161597becc335dce180010d0d4808c98
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-20 08:48:54 -0700

    a push

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
 /**
-  *
+  * free_grid - frees the memory of a 2d array
+  * @grid: 2d array
+  * @height: height of grid
+  * Return: only if grid == null
   */
 
 void free_grid(int **grid, int height)
 {
 	int i;
+	
+	if (grid == NULL)
+		return;
 
 	for (i = height; i >= 0; i--)
 		free(grid[i]);
