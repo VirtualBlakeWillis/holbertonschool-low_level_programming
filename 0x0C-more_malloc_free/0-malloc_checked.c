4bfa4baa68688b69bd7210b5acddfb76533ec015
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-21 09:12:49 -0700

    concatonate, with n!

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
-  *
+  * malloc_checked - allocate memory, exit if fails
+  * @b: number of bytes fo allocate
+  * Return: void pointer
   */
 
 void *malloc_checked(unsigned int b)
 {
-	int *s;
+	void *s;
+
 	s = malloc(b);
 
 	if (s == NULL)
 		exit(98);
-	else
-		return (s);
+
+	return (s);
 }
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+  * string_nconcat - concatonate n bytes of string2, onto string1
+  * @s1: string 1
+  * @s2: string 2
+  * @n: n bytes
+  * Return: pointer to new string
+  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int s1len, s2len, i;
+	char *s;
+
+	
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	s1len = strlen(s1);
+	s2len = strlen(s2);
+	if ((int) n <= s2len)
+		s2len = n;
+		
+	s = malloc(s1len + s2len + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < s1len)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (i < s2len)
+	{
+		s[i + s1len] = s2[i];
+		i++;
+	}
+
+	s[s1len + s2len] = '\0';
+
+	return (s);
+
+
+}
