d905b18c6509d3fa68c52fcfacbb75940980cb71
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-21 16:50:03 -0700

    betty is happy

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int s1len, s2len, i;
 	char *s;
-
 	char *junk;
 
 	junk = malloc(5);
@@ -20,7 +19,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
