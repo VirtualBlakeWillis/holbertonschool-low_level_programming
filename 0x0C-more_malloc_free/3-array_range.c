da997b1015f3d7322a6dd57ee78aeb06ca163a4a
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-21 09:34:10 -0700

    yay

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  *
+  */
+
+int *array_range(int min, int max)
+{
+	
+	int len = 0, i, *ptr;
+
+	if (min > max)
+		return (NULL);
+
+	len = max - min;
+
+	ptr = malloc((len * sizeof(int)) + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	i = 0;
+	while ((min + i) <= max)
+	{
+		ptr[i] = min + i;
+		i++;
+	}
+
+	return (ptr);
+
+}
