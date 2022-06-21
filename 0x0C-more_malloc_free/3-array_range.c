11a56953a5acc93800fd5924224568ca3868252f
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-21 11:49:21 -0700

    betty

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
-  *
+  * array_range - create array of all numbers from min to max, inclusive
+  * @min: number to start array at
+  * @max: number to end array at
+  * Return: pointer to int array
   */
 
 int *array_range(int min, int max)
 {
-	
 	int len = 0, i, *ptr;
 
 	if (min > max)
