baa4f03aaa1d5af6926c1d86ab6b49b86422373c
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-22 08:45:56 -0700

    printing my shiz

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
 /**
   * struct dog - a dog!
   * @name: name
@@ -15,5 +20,6 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
