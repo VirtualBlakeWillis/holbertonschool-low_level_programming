eedee89589c9cb797ed22257577f01159574b522
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-22 10:14:00 -0700

    return if null

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,7 +1,16 @@
 #include "dog.h"
 
+/**
+  * free_dog - free memeory allocated for a dog structure
+  * @d: dog to free
+  * Return: if null
+  */
+
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
+
 	free(d->name);
 	free(d->owner);
 	free(d);
