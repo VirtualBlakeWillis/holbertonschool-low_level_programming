ab8bdff22ffeb54503719f11847301d1f316a226
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-22 08:35:37 -0700

    doggie is named

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+  * init_dog - create a dog from input
+  * @d: dog structure
+  * @name: name
+  * @age: age
+  * @owner: owner
+  * Return: void
+  */
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
