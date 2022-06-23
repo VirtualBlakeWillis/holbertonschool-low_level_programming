c36316f171f04243b06eabb3ff75edc52000f6ea
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-23 14:19:13 -0700

    betty is happy

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,10 @@
 #include "function_pointers.h"
 /**
-  *
+  * array_iterator - call function n times, where n is size of array
+  * @array: values to pass into function
+  * @size: size of array
+  * @action: function to call
+  * Return: end func if input is null
   */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
