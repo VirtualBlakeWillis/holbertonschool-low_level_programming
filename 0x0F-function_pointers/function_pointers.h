736bf899d7598cdfe55225470e49795f2053a3f7
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-23 13:15:02 -0700

    happier skele

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,7 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Project Prototypes */
+/* Project Prototypes  tasks 0-2 */
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 
 
