8eab374229994bcb64b2a9244616a797f4881790
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-23 13:56:41 -0700

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - call function that is passed in
+  * @name: input val for function to call
+  * @f: pointer to function to call
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
