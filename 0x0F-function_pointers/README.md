92d8004539d1822c5ae5c285390f01c6569b3e34
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-23 13:13:47 -0700

    happy skeleton

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+# Project 1135: 0x10. (0x0F, named wrong) C - Function Pointers
+## Link To Project: [Click Here!](https://intranet.hbtn.io/projects/1135)
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+/* Standard Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Project Prototypes */
+
+
+
+#endif
