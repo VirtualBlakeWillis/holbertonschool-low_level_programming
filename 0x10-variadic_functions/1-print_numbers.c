dce21aad3be9e1b70f3567e084ee0439a1c74dfb
VirtualBlakeWillis <bewillis101@gmail.com>
2022-06-28 10:13:00 -0700

    checking bad inpyts

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,20 +2,25 @@
 
 /**
   * print_numbers - print numbers, separated by separator
-  * @separator: what to print in between numbers
+  * @sep: what to print in between numbers
   * @n: # of numbers
   * Return: to exit on fail
   */
 
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_numbers(const char *sep, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list ap;
 
+	if (sep == NULL)
+		sep = "";
+	if (n <= 0)
+		return;
+
 	va_start(ap, n);
 
 	for (i = 0; i < (n - 1); i++)
-		printf("%d%s", va_arg(ap, int), separator);
+		printf("%d%s", va_arg(ap, int), sep);
 	printf("%d\n", va_arg(ap, int));
 
 }
