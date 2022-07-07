4937343c3b9c4e21818f03561523c0e12e51ffc1
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-07 09:26:56 -0700

    counts # of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * 
+ *
+ */
+size_t list_len(const list_t *h)
+{
+	int n = 0;
+	list_t const *temp = h;
+
+	while (temp)
+	{
+		temp = temp->next;
+		n++;
+	}
+
+	return (n);
+}
