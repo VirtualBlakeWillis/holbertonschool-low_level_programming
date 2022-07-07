fcf8cf7fa25546659ef0986667251bdf134d7912
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-07 09:37:28 -0700

    task 2 skele

---
+++ 0x12-singly_linked_lists/2-main.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/** 
+ * add_node - adds a new node to the start of a linked list
+ * @head: address of first node
+ * @str: content to add as new first node
+ * Return: address of new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t const *tmp;
+	list_t new;
+
+
+	return (new);
+}
