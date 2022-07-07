3685cd2cdaf6dd819ca0064786ef54c9ee1d6966
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-07 10:01:34 -0700

    guess

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/** 
+ * add_node - adds a new node to the start of a linked list
+ * @head: address of first node
+ * @str: content to add as new first node
+ * Return: address of new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+
+	newNode = malloc(sizeof(list_t));
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->next = *head;
+	newNode->str = strdup(str);
+	newNode->len = strlen(str);
+
+	head = &newNode;
+
+	return (*head);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,8 @@
 /* Standard Libraries */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
