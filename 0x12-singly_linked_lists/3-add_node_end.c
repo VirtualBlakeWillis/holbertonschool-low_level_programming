a407369c893520613c3aa3446f7291cb19016efe
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-07 11:43:06 -0700

    somehow works

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * 
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *temp = *head;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->next = NULL;
+	new->str = strdup(str);
+	new->len = strlen(str);
+
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new;
+	}
+	return (new);
+}
