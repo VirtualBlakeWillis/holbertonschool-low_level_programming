db6073852758598a6bf682b114aa8c4ee27234e0
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-07 09:21:35 -0700

    prints a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,25 @@
 #include "lists.h"
 
 /**
- *
+ * print_list - prints a singly linked list
+ * @h: first pointer (head)
+ * Return: # of elements printed
  */
 
 size_t print_list(const list_t *h)
 {
-	int n;
+	int n = 0;
+	const list_t *tmp = h;
+
+	do {
+		if (tmp->str)
+			printf("[%d] %s\n", tmp->len, tmp->str);
+		else
+			printf("[0] (nil)\n");
+		n++;
+		tmp = tmp->next;
+
+	} while (tmp != NULL);
+
 	return (n);
 }
