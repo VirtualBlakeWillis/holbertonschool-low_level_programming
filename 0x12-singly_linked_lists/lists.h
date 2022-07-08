c27142d5e19e0c55f496273cc7f56ff222ad19a7
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-08 08:54:57 -0700

    betty header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,9 +15,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 /* function prototypes */
 size_t print_list(const list_t *h);
