ff257439860fcf4429ffcc7de52cca7bf1918195
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-08 08:47:19 -0700

    null case

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,8 +11,8 @@ size_t print_list(const list_t *h)
 	int n = 0;
 	const list_t *tmp = h;
 
-	if (h = NULL)
-		return (NULL);
+	if (tmp == NULL)
+		return (n);
 	do {
 		if (tmp->str)
 			printf("[%d] %s\n", tmp->len, tmp->str);
