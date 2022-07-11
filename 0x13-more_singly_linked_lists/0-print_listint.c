4a562fab48603ec6d7e724a6db05f879420dac85
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-11 09:40:03 -0700

    betty is happy

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,16 @@
 #include "lists.h"
 
 /**
+ * print_listint - print contents of list, return # of elements
+ * @h: head of list
+ * Return: # of items in list
  */
 
 size_t print_listint(const listint_t *h)
 {
 	int n = 0;
-	for(; h; n++)
+
+	for (; h; n++)
 	{
 		printf("%i\n", h->n);
 		h = h->next;
