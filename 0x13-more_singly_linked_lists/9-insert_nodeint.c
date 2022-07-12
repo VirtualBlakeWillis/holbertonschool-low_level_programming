3b3e5366b0476e270bf2935b38f344098922859d
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-12 13:30:16 -0700

    null check at end of list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp = temp->next;
 	}
-
+	if (temp == NULL)
+		return (NULL);
 	new->n = n;
 	new->next = temp->next;
 
