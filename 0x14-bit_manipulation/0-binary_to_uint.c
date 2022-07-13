999e2e822915af85de08524ee6d48d4f1a51868b
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-13 12:02:01 -0700

    revert, using len = strlen

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,14 +8,15 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int total = 0, i = 0, power, n;
+	unsigned int total = 0, len, i = 0, power, n;
 
 	if (b == NULL)
 		return (0);
 
-	power = _pow(2, (strlen(b) - 1));
+	len = strlen(b);
+	power = _pow(2, (len - 1));
 
-	while (i < strlen(b))
+	while (i < len)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
