4f482db942f92d3066137a5c0c1d73a51b6bf531
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-12 18:19:53 -0700

    0th node case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (new == NULL)
 		return (NULL);
-
+	if (idx == 0)
+	{
+		new->next = temp;
+		*head = new;
+		return (new);
+	}
 	for (count = 1; count < idx; count++)
 	{
 		if (temp->next == NULL)
