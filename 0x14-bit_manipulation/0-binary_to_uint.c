c5876db55c43415bab990336e5a53c93afb13c25
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-13 12:00:24 -0700

    strlen, not len

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,15 +8,14 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int total = 0, len, i = 0, power, n;
+	unsigned int total = 0, i = 0, power, n;
 
 	if (b == NULL)
 		return (0);
 
-	len = strlen(b);
-	power = _pow(2, (len - 1));
+	power = _pow(2, (strlen(b) - 1));
 
-	while (i < len)
+	while (i < strlen(b))
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
@@ -24,7 +23,7 @@ unsigned int binary_to_uint(const char *b)
 		n = (unsigned int)b[i] - '0';
 
 		total += n * power;
-		power = power / 2;
+		power /= 2;
 		i++;
 	}
 
