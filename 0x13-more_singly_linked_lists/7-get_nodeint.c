986f0af7170f210552d6d7b51edccae2b203f4cc
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-12 18:14:44 -0700

    null case?

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int n;
 
 	for (n = 0; n < index; n++)
+	{
+		if (temp->next == NULL)
+			return (NULL);
 		temp = temp->next;
-
+	}
 	return (temp);
 }
