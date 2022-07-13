687ee1cd452cf96acbb121759b6ee08ada800eab
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-12 17:39:40 -0700

    correct syntax

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,17 +6,17 @@
 #include <string.h>
 
 /**
- * struct listint_t - singly linked list
+ * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  *
  */
-typedef struct listint_t
+typedef struct listint_s
 {
 	int n;
-	struct listint_t *next;
+	struct listint_s *next;
 } listint_t;
 
 /* function prototypes */
