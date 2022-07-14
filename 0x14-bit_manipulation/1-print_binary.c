e66bc7144587fbf7db9d148e5785b9a0f430ed33
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-14 12:16:43 -0700

    betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- *
+ * print_binary - convert base 10 input to binary
+ * @n: base 10 number
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
 	unsigned long int binary = n;
 
 	if (n > 1)
-		print_binary(n>>1);
+		print_binary(n >> 1);
 
 	_putchar((binary & 1) + '0');
-	return;
 }
