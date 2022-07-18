7f96347ec953a1ad47ab89dcaa36913523ac4398
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-18 13:48:10 -0700

    correct return val

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == 0)
 		return (0);
 
-	read(fd, buf, letters);
+	c = read(fd, buf, letters);
 	if (write(1, buf, letters) == -1)
 		return (0);
 
