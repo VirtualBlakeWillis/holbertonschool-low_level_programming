0db7b3628edfb616d08b3295a60512f38e379bf9
VirtualBlakeWillis <bewillis101@gmail.com>
2022-07-18 14:01:08 -0700

    free _ close

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,18 +15,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buf = malloc(c * sizeof(char));
 
 	if (buf == NULL || filename == NULL)
+	{
+		free (buf);
 		return (0);
-
+	}
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
+	{
+		close(fd);
 		return (0);
-
+	}
 	c = read(fd, buf, c);
 
 	if (write(1, buf, c) != c)
+	{
+		close(fd);
+		free(buf);
 		return (0);
-
+	}
 	close(fd);
 	free(buf);
 	return (c);
