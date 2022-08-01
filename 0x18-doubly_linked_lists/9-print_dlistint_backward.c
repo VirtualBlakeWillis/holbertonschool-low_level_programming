0a60c777ce9a215898895b5c3c56fff0491131b8
VirtualBlakeWillis <bewillis101@gmail.com>
2022-08-01 12:54:07 -0700

    moving backwrds!

---
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint_backward - print a doubly liked list backwards
+ *
+ * @head: head of list
+ * Return: # of nodes
+ */
+
+size_t print_dlistint_backward(const dlistint_t *head)
+{
+	int n = 0;
+
+	while (head->next != NULL)
+		head = head->next;
+
+	while (head != NULL)
+	{
+		printf("%d\n", head->n);
+		head = head->prev;
+		n++;
+	}
+	return (n);
+}
