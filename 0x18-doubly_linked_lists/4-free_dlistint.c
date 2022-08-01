3f7bc68c58a2099f749fc9827814031d9fac3c54
VirtualBlakeWillis <bewillis101@gmail.com>
2022-08-01 12:41:51 -0700

    frees a list

---
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free a doubly linked list
+ *
+ * @head: head of list
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head->next != NULL)
+	{
+		head = head->next;
+		tmp = head->prev;
+		free(tmp);
+	}
+	free(head);
+}
