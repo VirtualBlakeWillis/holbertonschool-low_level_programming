266e82d3e6e1fded46322f47c106eaba2a94af7a
VirtualBlakeWillis <bewillis101@gmail.com>
2022-08-03 13:16:35 -0700

    print hash function

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ *
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i, isNotFirst = 0;
+	hash_node_t *temp;
+
+	printf("{");
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		if (ht->array[i] != NULL)
+		{
+			while (temp)
+			{
+				if (isNotFirst == 1)
+					printf(", ");
+				printf("'%s':'%s'", temp->key, temp->value);
+				isNotFirst = 1;
+				temp = temp->next;
+			}
+		}
+	}
+
+	printf("}\n");
+}
