0f3daf9927c360285813c8d206816d322b7b4e9b
VirtualBlakeWillis <bewillis101@gmail.com>
2022-08-04 11:14:40 -0700

    betty

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,7 +8,7 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	long unsigned int i = 0;
+	unsigned long int i = 0;
 	hash_node_t *temp, *head;
 
 	if (!ht)
@@ -27,9 +27,7 @@ void hash_table_delete(hash_table_t *ht)
 				free(temp);
 			}
 			free(head);
-
 		}
-		
 	}
 	free(ht->array);
 	free(ht);
