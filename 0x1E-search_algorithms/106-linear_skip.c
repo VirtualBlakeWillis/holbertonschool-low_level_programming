6026e98c2f20448564baad41e9f687133ddda983
virtualblakewillis <bewillis101@gmail.com>
2023-03-21 10:45:49 -0600

    linear skip

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,50 @@
+#include "search_algos.h"
+
+
+/**
+ * linear_skip - linear search function for a skip list
+ *
+ * @list: singly linked list to search
+ * @value: value to search for
+ * Return: pointer to first node where value is located, or NULL
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+    int r;
+    skiplist_t *prev = list;
+    skiplist_t *next = list->express;
+
+    if (!list)
+        return (NULL);
+
+
+    while (prev->express && prev->n < value)
+    {
+        printf("Value checked at index [%zu] = [%d]\n", next->index, next->n);
+        if (next->n >= value)
+            break;
+        prev = next;
+        next = next->express;
+    }
+    if (!next)
+    {
+        next = prev;
+        while(next->next)
+            next = next->next;
+    }
+    printf("Value found between indexes [%zu] and [%zu]\n", prev->index, next->index);
+
+    size_t upper = next->index;
+    while(prev->index <= upper && prev->n <= value)
+    {
+        printf("Value checked at index [%zu] = [%d]\n", prev->index, prev->n);
+        if (prev->n == value)
+            return prev;
+        
+        if (prev->index == upper)
+            break;
+        prev = prev->next;
+
+    }
+    return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,9 +3,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
+
+
 
 int advanced_binary(int *array, size_t size, int value);
 int recursive_binary_search(int *array, int left, int right, int value);
 void print_array(const int *array, int start, int end);
+
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
